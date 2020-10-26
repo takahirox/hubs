23e4316c83c30b25581642daf9f3edb515be872b
Takahiro <hogehoge@gachapin.jp>
2020-10-26 12:26:02 -0700

    Discord fullscreen

--- src/utils/fullscreen.js
+++ src/utils/fullscreen.js
@@ -4,6 +4,7 @@ let hasEnteredFullScreenThisSession = false;
 
 function shouldShowFullScreen() {
   // Disable full screen on iOS, since Safari's fullscreen mode does not let you prevent native pinch-to-zoom gestures.
+  return false;
   return (
     (AFRAME.utils.device.isMobile() || AFRAME.utils.device.isMobileVR()) &&
     !AFRAME.utils.device.isIOS() &&
